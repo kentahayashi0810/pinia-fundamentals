16adb39e88f63038500834fc6f87a0ee81a13ae2
kentahayashi <kenta0810hayashi@gmail.com>
2023-08-20 00:25:19 +0200

    Init git

---
+++ src/components/TodoList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useTodoListStore } from "../stores/todoList.js";
+
+const store = useTodoListStore();
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in store.todoList">
+      {{ todo.item
+      }}<button @click="store.deleteTodo(todo.id)">Delete Item</button>
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped></style>
